520b05ff9228d54a659a67883f094da1c3355aa0
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:41:05 +0530

    create post entity

--- src/main/java/com/vaibhav/anonymousforum/entities/Post.java
+++ src/main/java/com/vaibhav/anonymousforum/entities/Post.java
@@ -1,4 +1,88 @@
 package com.vaibhav.anonymousforum.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String content, User user, List<Comment> comments) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.comments = comments;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", user=" + user +
+                ", comments=" + comments +
+                '}';
+    }
 }
