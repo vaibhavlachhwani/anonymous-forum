60633abe878e0b941306b769c9ddbdcfdf72a5e5
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:57:36 +0530

    implement post service

---
+++ src/main/java/com/vaibhav/anonymousforum/services/PostService.java
@@ -0,0 +1,27 @@
+package com.vaibhav.anonymousforum.services;
+
+import com.vaibhav.anonymousforum.entities.Post;
+import com.vaibhav.anonymousforum.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+}
