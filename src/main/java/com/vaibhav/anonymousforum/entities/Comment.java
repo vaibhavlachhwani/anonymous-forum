1e6a6aa2a6df14d08807aedfb5b0fd6426a615c8
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:41:25 +0530

    create comment entity

--- src/main/java/com/vaibhav/anonymousforum/entities/Comment.java
+++ src/main/java/com/vaibhav/anonymousforum/entities/Comment.java
@@ -1,4 +1,75 @@
 package com.vaibhav.anonymousforum.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, String content, Post post, User user) {
+        this.id = id;
+        this.content = content;
+        this.post = post;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", post=" + post +
+                ", user=" + user +
+                '}';
+    }
 }
