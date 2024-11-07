a71c95a27b1df419c800dfec94c90deb9199babe
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 23:51:23 +0530

    add endpoint to comment on a specific post.

--- src/main/java/com/vaibhav/anonymousforum/controllers/PostController.java
+++ src/main/java/com/vaibhav/anonymousforum/controllers/PostController.java
@@ -1,7 +1,14 @@
 package com.vaibhav.anonymousforum.controllers;
 
+import com.vaibhav.anonymousforum.dtos.CommentDTO;
+import com.vaibhav.anonymousforum.dtos.CommentRequestDTO;
 import com.vaibhav.anonymousforum.dtos.PostDTO;
+import com.vaibhav.anonymousforum.entities.Comment;
 import com.vaibhav.anonymousforum.entities.Post;
+import com.vaibhav.anonymousforum.entities.User;
+import com.vaibhav.anonymousforum.repositories.CommentRepository;
+import com.vaibhav.anonymousforum.repositories.PostRepository;
+import com.vaibhav.anonymousforum.repositories.UserRepository;
 import com.vaibhav.anonymousforum.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -16,6 +24,15 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     // Get all posts
     @GetMapping
@@ -35,5 +52,29 @@ public class PostController {
     public ResponseEntity<PostDTO> createPost(@RequestBody Post post) {
         PostDTO createdPost = postService.createPost(post);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
+
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<CommentDTO> addCommentToPost(@PathVariable Long id, @RequestBody CommentRequestDTO commentRequestDTO) {
+        Optional<Post> postOptional = postRepository.findById(id);
+        if (!postOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Post post = postOptional.get();
+
+        // Assume we have a method to get the current user
+        User user = userRepository.findById(commentRequestDTO.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Create and save the new comment
+        Comment comment = new Comment();
+        comment.setContent(commentRequestDTO.getContent());
+        comment.setPost(post);
+        comment.setUser(user);  // Set the user who made the comment
+        comment = commentRepository.save(comment);
+
+        // Convert saved comment to CommentDTO
+        CommentDTO commentDTO = new CommentDTO(comment);
+        return ResponseEntity.ok(commentDTO);
     }
 }
