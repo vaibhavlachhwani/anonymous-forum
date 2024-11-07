ab8625d5d6a33388ddfc1dc46533909b1d088fdf
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:54:56 +0530

    implement user service

--- src/main/java/com/vaibhav/anonymousforum/repositories/UserRepository.java
+++ src/main/java/com/vaibhav/anonymousforum/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.vaibhav.anonymousforum.repositories;
 
+import com.vaibhav.anonymousforum.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/com/vaibhav/anonymousforum/services/UserService.java
@@ -0,0 +1,27 @@
+package com.vaibhav.anonymousforum.services;
+
+import com.vaibhav.anonymousforum.entities.User;
+import com.vaibhav.anonymousforum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
