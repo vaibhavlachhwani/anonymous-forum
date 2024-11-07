4ffaba2426ecb30e18b6f1d4ae0a25f4eb054642
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:59:28 +0530

    implement comment service

---
+++ src/main/java/com/vaibhav/anonymousforum/services/CommentService.java
@@ -0,0 +1,27 @@
+package com.vaibhav.anonymousforum.services;
+
+import com.vaibhav.anonymousforum.entities.Comment;
+import com.vaibhav.anonymousforum.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Comment getCommentById(Long id) {
+        return commentRepository.findById(id).orElse(null);
+    }
+
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+}
