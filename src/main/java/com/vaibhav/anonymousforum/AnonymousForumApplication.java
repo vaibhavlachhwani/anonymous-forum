b479bab468512c757872bfbdfc50aae0efda0245
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 17:18:39 +0530

    initialize project

---
+++ src/main/java/com/vaibhav/anonymousforum/AnonymousForumApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.anonymousforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vaibhav/anonymousforum/AnonymousForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhav.anonymousforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
