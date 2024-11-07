e17baee45fbaa983d63fa4123ac1bd5c8c1c17c1
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 21:28:50 +0530

    implement user controller

---
+++ src/main/java/com/vaibhav/anonymousforum/services/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.vaibhav.anonymousforum.services.controllers;
+
+import com.vaibhav.anonymousforum.entities.User;
+import com.vaibhav.anonymousforum.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
