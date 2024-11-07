1bca95620bdfe4b1403ee0a4b59027eb3356cdba
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-07 22:47:09 +0530

    update UserService to use UserDTO

--- src/main/java/com/vaibhav/anonymousforum/services/UserService.java
+++ src/main/java/com/vaibhav/anonymousforum/services/UserService.java
@@ -1,11 +1,13 @@
 package com.vaibhav.anonymousforum.services;
 
+import com.vaibhav.anonymousforum.dtos.UserDTO;
 import com.vaibhav.anonymousforum.entities.User;
 import com.vaibhav.anonymousforum.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -13,15 +15,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(UserDTO::new)
+                .collect(Collectors.toList());
     }
 
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
+    public UserDTO getUserById(Long id) {
+        return userRepository.findById(id)
+                .map(UserDTO::new)
+                .orElse(null);
     }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    public UserDTO createUser(User user) {
+        User savedUser = userRepository.save(user);
+        return new UserDTO(savedUser);
     }
 }
