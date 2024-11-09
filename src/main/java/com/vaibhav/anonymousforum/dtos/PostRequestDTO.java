b3a67890a3a633258eb498b318226bfc8cc399c9
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-09 15:07:13 +0530

    create PostRequestDTO

---
+++ src/main/java/com/vaibhav/anonymousforum/dtos/PostRequestDTO.java
@@ -0,0 +1,40 @@
+package com.vaibhav.anonymousforum.dtos;
+
+public class PostRequestDTO {
+    private String title;
+    private String content;
+    private String username;
+    private String password;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
